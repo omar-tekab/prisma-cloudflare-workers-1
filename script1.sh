a6a71ae910a58c15b26206baa83d2f0826b2834e
Omar Abdelkafi <omar.abdelkafi@tekab.dev>
2023-09-12 09:58:36 +0000

    test

---
+++ script1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Specify the text you want to add
+text_to_add="This text was added automatically."
+
+# Specify the file path
+file_path="omar.txt"
+
+# Check if the file exists
+if [ -f "$file_path" ]; then
+  # Append the text to the file
+  echo "$text_to_add" >> "$file_path"
+  echo "Text added to $file_path."
+else
+  echo "The file $file_path does not exist."
+fi
